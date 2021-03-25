c16c238148b4a03d46daf1201db7a0418dcb7c09
adelinel <adeline.latruwe@gmail.com>
2021-03-25 15:17:31 -0700

    created tables and seed file

---
+++ migrations/seed.tech_list.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+INSERT INTO tech_list (name, icon) VALUES
+('css', '<img src="https://img.icons8.com/color/48/000000/css3.png"/>'),
+('html', '<img src="https://img.icons8.com/color/64/000000/html-5.png"/>'),
+('javascript', '<img src="https://img.icons8.com/color/48/000000/javascript.png"/>'),
+('python', '<img src="https://img.icons8.com/color/48/000000/python.png"/>'),
+('react', '<img src="https://img.icons8.com/color/48/000000/react-native.png"/>'),
+('node.js', '<img src="https://img.icons8.com/color/48/000000/nodejs.png"/>'),
+('postgresql', '<img src="https://img.icons8.com/color/48/000000/postgreesql.png"/>'),
+('c++', '<img src="https://img.icons8.com/color/48/000000/c-plus-plus-logo.png"/>'),
+('java', '<img src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png"/>');
+
+COMMIT;
