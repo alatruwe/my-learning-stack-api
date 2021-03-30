bd0414f70ccca37d1211d5c096f524f0ae8e176d
adelinel <adeline.latruwe@gmail.com>
2021-03-30 13:20:52 -0700

    resolved merge conflicts

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 const loginRouter = require("./login/login-router");
 const signupRouter = require("./signup/signup-router");
+const dashboardRouter = require("./dashboard/dashboard-router");
+const profileRouter = require("./profile/profile-router");
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(cors());
 
 app.use("/api/login", loginRouter);
 app.use("/api/signup", signupRouter);
+app.use("/api/profile", profileRouter);
+app.use("/api/dashboard", dashboardRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
