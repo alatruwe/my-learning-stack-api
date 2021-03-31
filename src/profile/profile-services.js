db67bec54fdc2dd6ae2c8cf4dd9061547fade55f
adelinel <adeline.latruwe@gmail.com>
2021-03-31 11:26:14 -0700

    remove unused code

--- src/profile/profile-services.js
+++ src/profile/profile-services.js
@@ -7,26 +7,6 @@ const ProfileService = {
       .then(([profile]) => profile);
   },
 
-  getAllTech(db) {
-    return db.from("tech_list").select("name", "id");
-  },
-
-  getTechId(db, name) {
-    return db
-      .from("tech_list")
-      .select("id")
-      .where("name", name)
-      .then(([id]) => id);
-  },
-
-  insertNewTech(db, newTech) {
-    return db
-      .insert(newTech)
-      .into("tech_list")
-      .returning("*")
-      .then(([tech]) => tech);
-  },
-
   getUserprofile(db, user_id) {
     return db
       .where("profiles.user_id", user_id)
