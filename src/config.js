f56ea1d18eebcd82ef2958433e3ddf4642c88113
adelinel <adeline.latruwe@gmail.com>
2021-04-06 11:26:20 -0700

    updated CORS

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgresql://adelinelatruwe@localhost/my-learning-stack",
-  TEST_DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgresql://adelinelatruwe@localhost/my-learning-stack-test",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000/",
+  DATABASE_URL: process.env.DATABASE_URL,
+  TEST_DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "120s",
 };
